ee23c66bc3247a39295b809c7083f9fc53dd1691
maurograndaoramos <mauro.dgramos@gmail.com>
2024-04-02 21:52:11 +0100

    [PATCH] Update files for aula5

--- aula5/run.py
+++ aula5/run.py
@@ -1,8 +1,17 @@
 import click
 
+filename = "compras.txt"
+
 @click.command()
 def hello():
     click.echo("Hello", color=True)
+
+@click.command()
+@click.argument("Product")
+def add(product:str):
+    breakpoint
+    with open(filename, "a") as file:
+        file.write(f"{product}\n")
 
 if __name__ == "__main__":
     hello()
--- aula5/tests_cli.py
+++ aula5/tests_cli.py
@@ -13,8 +13,8 @@ def test_call_add():
     assert result.exit_code is 0
     assert "CARROTS added" in result.output
 
-def test_call_remove():
-    runner = CliRunner()
-    result = runner.invoke(hello)
-    assert result.exit_code is 0
-    assert "Hello" in result.output
+# def test_call_remove():
+#     runner = CliRunner()
+#     result = runner.invoke(hello)
+#     assert result.exit_code is 0
+#     assert "Hello" in result.output
