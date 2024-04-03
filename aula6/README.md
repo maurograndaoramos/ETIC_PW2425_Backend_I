33adefd3d3b7a1fa27bd767398c38f95cbf717b1
maurograndaoramos <mauro.dgramos@gmail.com>
2024-04-03 21:15:23 +0100

    [FEAT] Adding info from aula6

---
+++ aula6/README.md
@@ -0,0 +1,2 @@
+# ETIC_Bot
+A simple Discord Bot that allows the user to write /input ping and receive a response (Pong)
--- aula6/src/run.py
+++ aula6/src/run.py
@@ -1,17 +1,24 @@
 import discord
 from discord.ext import commands
+# from discord.commands import slash_command
+import os
 
-client = commands.Bot(command_prefix="/input ", intents=discord.Intents.all())
+client = commands.Bot(command_prefix='/input ', intents=discord.Intents.all())
+# intents = discord.Intents.default()
+# intents.message_content = True
+# bot = commands.Bot(command_prefix='/input ', intents=intents)
 
 @client.event
 async def on_ready():
-    print("pingpong is ready!")
-    print("----------------------")
+    print('pingpong app is ready!')
+
+# @slash_command(guild_ids=[NVM_THIS], description="Responds with pong")
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+    await ctx.send(f'Pong!')
 
+TOKEN = os.getenv('not/gonna/dox/my/env/token.txt')
 
 client.run('TOKEN')
 
